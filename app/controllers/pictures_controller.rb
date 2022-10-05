f15d04a451921bab09394691d4f4a50dabdf379d
Ignacio Zanotto <ignaciozanotto@gmail.com>
2022-10-05 15:33:39 -0300

    setting cloudinary

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,3 +1,48 @@
 class PicturesController < ApplicationController
-  
+  def index
+    @pictures = Picture.all
+  end
+
+  def show
+    @picture = Picture.where(params[:id])
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # def edit
+  #   authorize @assistant
+  # end
+
+  # def update
+  #   authorize @assistant
+  #   if @assistant.update(assistant_params)
+  #     redirect_to assistant_path(@assistant)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
+
+  def destroy
+    @picture = Picture.where(params[:id])
+    @picture.destroy
+    redirect_to chatrooms_path
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:detail, :photo)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,7 @@ require "open-uri"
 
 User.destroy_all
 
+Us = Picture.new(detail: "foto nossa")
+Us.photo.attach(io: URI.open("https://images.unsplash.com/photo-1639619628924-eced0acbab4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=583&q=80"), filename: "assistant12.png", content_type: "image/jpeg")
+
 puts "Data Base CLEANED"
